02447052eaa818640ee72b8c3940942e75448729
Cem Yılmaz <yilmazcem1122@gmail.com>
2025-02-11 11:50:31 +0300

    C# Store Crud MVC projesi tamamlandi

---
+++ StoreCrudMVC/Controllers/ProductsController.cs
@@ -0,0 +1,176 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StoreCrudMVC.Models;
+using StoreCrudMVC.Services;
+
+namespace StoreCrudMVC.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly ApplicationDbContext context;
+        private readonly IWebHostEnvironment webHostEnvironment;
+
+        public ProductsController(ApplicationDbContext context, IWebHostEnvironment environment)
+        {
+            this.context = context;
+            this.webHostEnvironment = environment;
+        }
+        public IActionResult Index()
+        {
+            var products = context.Products.OrderByDescending(p => p.Id).ToList();  // burada db den products ürünlerini çekip listeye çeviriyor  Order By Descending kullanınca son ürünler en üstte çıkıyor
+            return View(products);
+        }
+
+
+        public IActionResult Create()
+        {
+            return View();
+
+        }
+
+        [HttpPost]
+        public IActionResult Create(ProductDto productDto)
+        {
+            if( productDto.ImageFile == null)
+            {
+                ModelState.AddModelError("ImageFile", "The image file is required");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return View(productDto);
+                   
+            }
+
+            // image kaydetme işlemi
+            string newFileName = DateTime.Now.ToString("yyyyMMddHHmmssfff");
+            newFileName += Path.GetExtension(productDto.ImageFile!.FileName);
+
+            string imageFullPath = webHostEnvironment.WebRootPath + "/products/" + newFileName;
+            using (var stream = System.IO.File.Create(imageFullPath))
+            {
+                productDto.ImageFile.CopyTo(stream);
+            }
+
+            // Productı databaseye kaydetme
+
+            Product product = new Product()
+            {
+                Name = productDto.Name,
+                Brand = productDto.Brand,
+                Category = productDto.Category,
+                Price = productDto.Price,
+                Description = productDto.Description,
+                ImageFileName = newFileName,
+                CreatedAt = DateTime.Now
+
+            };
+
+            context.Products.Add(product);
+            context.SaveChanges();
+
+
+            return RedirectToAction("Index", "Products");
+
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var product = context.Products.Find(id);
+
+            if (product == null)
+            {
+                return RedirectToAction("Index", "Products");
+            }
+            var productDto = new ProductDto()
+            {
+                Name = product.Name,
+                Brand = product.Brand,
+                Category = product.Category,
+                Price = product.Price,
+                Description = product.Description,
+
+            };
+
+            ViewData["ProductId"] = product.Id;
+            ViewData["ImageFileName"] = product.ImageFileName;
+            ViewData["CreatedAt"] = product.CreatedAt.ToString("MM/dd/yyyy");
+
+            return View(productDto);
+
+        }
+        [HttpPost]
+        public IActionResult Edit(int id, ProductDto productDto)
+        {
+            var product = context.Products.Find(id);
+            if (product == null)
+            {
+                return RedirectToAction("Index", "Products");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                ViewData["ProductId"] = product.Id;
+                ViewData["ImageFileName"] = product.ImageFileName;
+                ViewData["CreatedAt"] = product.CreatedAt.ToString("MM/dd/yyyy");
+
+                return View(productDto);
+            }
+
+            // resimi güncellemek için
+
+            string newFileName = product.ImageFileName;
+            if (productDto.ImageFile != null)
+            {
+                newFileName = DateTime.Now.ToString("yyyyMMddHHmmssffff");
+                newFileName += Path.GetExtension(productDto.ImageFile.FileName);
+
+                string imageFullPath = webHostEnvironment.WebRootPath + "/products/" + newFileName;
+
+                using (var stream = System.IO.File.Create(imageFullPath))
+                {
+                    productDto.ImageFile.CopyTo(stream);
+                };
+
+                // eski resmi silme
+                string oldImageFullPath = webHostEnvironment.WebRootPath + "/products/" + product.ImageFileName;
+                System.IO.File.Delete(oldImageFullPath);
+
+               
+                
+
+            }
+            product.Name = productDto.Name;
+            product.Brand = productDto.Brand;
+            product.Category = productDto.Category;
+            product.Price = productDto.Price;
+            product.Description = productDto.Description;
+            product.ImageFileName = newFileName;
+
+            context.SaveChanges();
+
+            return RedirectToAction("Index", "Products");
+
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var product = context.Products.Find(id);
+
+            if (product == null)
+            {
+                return RedirectToAction("Index", "Products");
+            }
+
+            string imageFullPath = webHostEnvironment.WebRootPath + "/products" + product.ImageFileName;
+            System.IO.File.Delete(imageFullPath);
+
+            context.Products.Remove(product);
+            context.SaveChanges(true);
+
+            return RedirectToAction("Index", "Products");
+
+        }
+
+    }
+}
---
+++ StoreCrudMVC/Models/ProductDto.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace StoreCrudMVC.Models
+{
+    public class ProductDto
+    {
+        [Required,MaxLength(100)]
+        public string Name { get; set; } = "";
+
+        [Required, MaxLength(100)]
+        public string Brand { get; set; } = "";
+        [Required, MaxLength(100)]
+        public string Category { get; set; } = "";
+        [Required]
+        public decimal Price { get; set; }
+        [Required]
+        public string Description { get; set; } = "";
+
+        public IFormFile? ImageFile { get; set; } // Burada create ederken gerekli ama update ederken gerekli olmadığı için ? koyduk
+
+
+    }
+}
